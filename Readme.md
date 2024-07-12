62849bb460ac409552accffdb7bd6105bb7deff7
Nergiiz <151402997+Nergiiz@users.noreply.github.com>
2024-07-12 22:57:25 +0400

    first commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+<h1> sinema rezervasyon <h1>
+
+<h1> html, css, scss ve js kullanılarak tasarlanmıştır. <h1>
+
+<h2> ekran görüntüsü <h2>
+
+![](cinema.gif)
---
+++ js/helpers.js
@@ -0,0 +1,8 @@
+export const elements = {
+    container: document.querySelector(".container"),
+    infoText: document.querySelector(".info-text"),
+    selectedSeatCount: document.querySelectorAll(".seat .selected"),
+    select: document.getElementById("movie"),
+    count: document.getElementById("count"),
+    amount: document.getElementById("amount"),
+  };
